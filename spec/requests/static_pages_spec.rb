a2efb4fe2f896e9d75df4fe01d323de30011e7e4
nukui <nukui@kkcats.com>
2015-04-25 14:39:13 +0000

    modify

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Sample3'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample3')
+    end
+  end
+  describe "Hello page" do
+    it "shoud have content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+end
