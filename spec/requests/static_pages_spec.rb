e71c00f222f2da5a0d465bd27a4d0af0782fe56e
nukui <nukui@kkcats.com>
2015-04-26 13:31:18 +0000

    modify

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 
 describe "Static pages" do
   describe "Home page" do
-    it "should have the content 'Sample3'" do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample3')
+      expect(page).to have_content('Sample App')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
   end
   describe "Hello page" do
@@ -12,11 +16,19 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
